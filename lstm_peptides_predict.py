40671925afaa9e184cdf723e56bdd6c5a9eb4015
alexarnimueller <alex.mueller@pharma.ethz.ch>
2017-06-20 17:11:51 +0200

    changed code to one-hot vector representation in training & prediction

--- lstm_peptides_predict.py
+++ lstm_peptides_predict.py
@@ -7,42 +7,50 @@ from keras.models import load_model
 from modlamp.core import aa_weights, BaseSequence, save_fasta
 from progressbar import ProgressBar
 
-
-seq_num = 50  # number of sequences to generate, can be smaller because too short sequences are kicked out
+# ----------------- parameters to adapt ------------------
+seq_num = 10  # number of sequences to generate, can be smaller because too short sequences are kicked out
 seq_len = 15  # approximate length of generated sequences (afterwards joined and split at newlines)
 len_range = range(5, 51)  # range of accepted sequence lengths
-featlen = 32  # feature length, defined in training
-pwdir = '/Users/modlab/x/projects/LSTM/'  # working directory
+featlen = 22  # feature length, meaning how many amino acids are presented to the network at one step
+pwdir = '/Volumes/Platte1/x/projects/LSTM/'  # working directory
+# --------------------------------------------------------
 
-# # build translation dictionaries
+
+# get AA vocabulary for translation
 AAs = sorted(aa_weights().keys())
 AAs.append(' ')
 n_vocab = len(AAs)
-aa_to_int = dict((c, i) for i, c in enumerate(AAs))
-int_to_aa = dict((i, c) for i, c in enumerate(AAs))
 
 # load the trained model
-print('Loading and plotting model...')
-model = load_model(pwdir + "data/logs/best_model_15e.hdf5")
+print('Loading trained model...')
+model = load_model(pwdir + "data/logs/best_lstm_01-2.6122.hdf5")
 
 # ----------------- sequence generation ------------------
 print("Generating novel sequences...\n")
 pbar = ProgressBar()
 pep = BaseSequence(1)  # storage vehicle
-rs = 0
+rs = 0  # initiate random seed, changes for every sequence
 for s in pbar(range(seq_num)):
+    # generate random starting matrix X
     np.random.seed = rs
-    # generate random starting vector
-    pattern = np.random.rand(1, featlen, 1)[0]
+    X = np.zeros((1, featlen, n_vocab))
+    for i in range(featlen):
+        rand_aa = np.random.randint(0, n_vocab)  # get a random position of the vector
+        X[0, i, rand_aa] = 1.  # set this position to 1
+    
+    # generate sequence
     result = []
-    for i in range(seq_len):
-        x = np.reshape(pattern, (1, len(pattern), 1))
-        prediction = model.predict(x, verbose=0)
-        index = np.argmax(prediction)
-        result.append(int_to_aa[index])
-        pattern = np.append(pattern[1:], index / float(n_vocab))
-    pep.sequences.append(''.join(result))
-    rs += 1
+    for i in range(seq_len):  # generate AA by AA
+        prediction = model.predict(X, verbose=0)  # let the model predict the next most probable AA
+        i_max = np.argmax(prediction)  # index of highest probability in prediction
+        result.append(AAs[i_max])  # translate prediction back to the corresponding AA
+        tmp = np.zeros(n_vocab)  # temporary empty vector to transform prediction to a one-hot representation
+        tmp[i_max] = 1.  # AA to one-hot
+        # combine the prediction with X and use the new X to predict next AA
+        X = np.reshape(np.vstack((X[0, 1:], tmp)), (1, featlen, n_vocab))
+    
+    pep.sequences.append(''.join(result))  # join the AAs to a peptide sequence
+    rs += 1  # change the random seed for invoking the next sequence
 
 pep.sequences = ''.join(pep.sequences).split(' ')  # join all sequences and split them at the newline characters
 pep.sequences = [s for s in pep.sequences if len(s) in len_range]  # kick out all sequences that are too short
@@ -52,6 +60,6 @@ print("Generated sequences: %i \n" % len(pep.sequences))
 for s in pep.sequences:
     print(s)
     
-save_fasta(pep, pwdir + "data/produced.fasta")  # save the generated sequences
+save_fasta(pep, pwdir + "data/test.fasta")  # save the generated sequences
 
 print("\nDone.")
--- lstm_peptides_train.py
+++ lstm_peptides_train.py
@@ -1,8 +1,7 @@
+# -*- coding: utf-8 -*-
 """
 Training an LSTM model on antimicrobial peptides
 """
-
-from __future__ import print_function
 
 from time import time
 
@@ -12,17 +11,20 @@ from keras.callbacks import ModelCheckpoint
 from keras.initializers import RandomNormal
 from keras.layers import Dense, LSTM
 from keras.models import Sequential
-from keras.utils import np_utils
 from modlamp.core import aa_weights
 from modlamp.descriptors import GlobalDescriptor
 
 starttime = time()
 
-featlen = 32  # feature length
-step = 2  # AA step to move for cutting up sequences
-epochs = 15  # number of training epochs
-batch_s = 128  # batch size used for training
+# ----------------- parameters to adapt ------------------
+featlen = 22  # feature length (how many AA per step), here 6 * 3.6, representing approx. 6 turns in a helical peptide
+step = 2  # AA step size to move for enhancing sequence library
+epochs = 2  # number of training epochs
+n_units = 128  # number of LSTM units per layer
+batch_s = 64  # batch size used for training
 pwdir = '/Volumes/Platte1/x/projects/LSTM/'  # working directory
+# --------------------------------------------------------
+
 
 # reading sequence library to learn from
 print("Loading training sequences...")
@@ -30,59 +32,61 @@ d = GlobalDescriptor('A')
 with open(pwdir + 'data/training_sequences_noC.csv') as f:
     d.sequences = [s.strip() for s in f]
 
-# get rid of sequences containing unnatural AAs
+# get rid of sequences containing unnatural AAs and calculate length distribution
 d.keep_natural_aa()
-peptides = d.sequences
+d.length()
+print('\tSequence length distribution: %.1f ± %.1f' % (np.mean(d.descriptor), np.std(d.descriptor)))
 
-print('\ttotal number of sequences:', len(peptides))
-# peptext = '\n'.join(h.sequences)
-peptext = ' '.join(peptides)  # join the sequences through a space to a global training text
-print('\ttotal number of AAs:', len(peptext))
+print('\ttotal number of sequences: %i' % len(d.sequences))
+peptext = ' '.join(d.sequences)  # join the sequences through a space to a global training text
 
 # translation dictionaries int -> AA and vice versa
 AAs = sorted(aa_weights().keys())
 AAs.append(' ')
 n_vocab = len(AAs)
-print('\tnr. of different AAs (including space):', n_vocab)
-aa_to_int = dict((a, i) for i, a in enumerate(AAs))
-int_to_aa = dict((i, a) for i, a in enumerate(AAs))
+print('\tVocabulary size = nr. of different AAs (including space): %i' % n_vocab)
 
-# cut the text in semi-redundant sequences of featlen AA
+# generate one-hot vector representation
+aa_to_hot = dict()
+for i, a in enumerate(AAs):
+    v = np.zeros(n_vocab)
+    v[i] = 1
+    aa_to_hot[a] = v
+
+# cut the text in semi-redundant sequences of featlen AA in a one-hot vector representation
 X = []
 y = []
 for i in range(0, len(peptext) - featlen, step):
     seq_in = peptext[i: i + featlen]  # training data (-> X)
     seq_out = peptext[i + featlen]  # target data (-> y)
-    X.append([aa_to_int[char] for char in seq_in])
-    y.append(aa_to_int[seq_out])
+    X.append([aa_to_hot[char] for char in seq_in])
+    y.append(aa_to_hot[seq_out])
 n_patterns = len(X)
-print('\tnr. of sequence patterns:', n_patterns)
+print('\tnr. of sequence patterns: %i' % n_patterns)
 
 # reshape X to [samples, steps, features]
-print('Vectorization...')
-X = np.reshape(X, (n_patterns, featlen, 1))
-# normalization
-X = X / float(n_vocab)
-# one hot encoding of the target
-y = np_utils.to_categorical(y)
+X = np.reshape(X, (n_patterns, featlen, n_vocab))
+y = np.reshape(y, (n_patterns, n_vocab))
+print('\nShape of X: %s' % str(X.shape))
+print('Shape of y: %s' % str(y.shape))
 
 # build the deep LSTM model
-print('Building model...')
+print('\nBuilding model...')
 weight_init = RandomNormal(mean=0.0, stddev=0.05, seed=None)  # initialize weights randomly between -0.05 and 0.05
 model = Sequential()
-model.add(LSTM(512, input_shape=(X.shape[1], X.shape[2]),
+model.add(LSTM(n_units, input_shape=(X.shape[1], X.shape[2]),
                return_sequences=True,
                kernel_initializer=weight_init,
                use_bias=True,
                bias_initializer='zeros',
                unit_forget_bias=True))
-model.add(LSTM(512,
+model.add(LSTM(n_units,
                return_sequences=True,
                kernel_initializer=weight_init,
                use_bias=True,
                bias_initializer='zeros',
                unit_forget_bias=True))
-model.add(LSTM(512,
+model.add(LSTM(n_units,
                kernel_initializer=weight_init,
                use_bias=True,
                bias_initializer='zeros',
@@ -115,6 +119,6 @@ ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 ax.set_xlim([0, len(losses)])
 plt.legend(loc='best')
-plt.savefig(pwdir + 'figures/loss_plot_best.pdf')
+plt.savefig(pwdir + 'figures/loss_plot_%.2f.pdf' % losses[-1])
 
-print("Duration in minutes: ", (time() - starttime) / 60.)
+print("Duration in minutes: %.2f" % ((time() - starttime) / 60.))
