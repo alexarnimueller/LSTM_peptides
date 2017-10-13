150d2dcf33d3f6bb99cc43c052fd1f41f376a221
Alex Müller <alexarnimueller@gmail.com>
2017-10-13 11:04:23 +0200

    temp script, csv2fasta, plot predictions

---
+++ sample.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Script to sample with different temperatures from a trained model.
+Need to turn off 'ask' when initializing model
+"""
+
+import os
+
+sample = 1000
+temps = [3.5, 4.]
+name = 'desperate'
+sepoch = 249
+l = 1
+n = 64
+d = 0.2
+
+modfile = name + '/checkpoint/model_epoch_%i.hdf5' % sepoch
+
+for t in temps:
+    print("\nSampling %i sequences at %.1f temperature..." % (sample, t))
+    cmd = "python ~/PycharmProjects/LSTM_peptides/LSTM_peptides.py --train False --modfile %s --layers %i " \
+          "--neurons %i --dropout %.1f --temp %.1f --sample %i --name %s" % \
+          (modfile, l, n, d, t, sample, name)
+    os.system(cmd)
