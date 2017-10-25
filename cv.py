f1fe7667823f8207cd76d7266dd84ecb8358b5fa
Alex Müller <alex.mueller@pharma.ethz.ch>
2017-10-25 06:25:49 +0200

    cv w40 reslt and analysis py file

--- cv.py
+++ cv.py
@@ -7,14 +7,14 @@ Script to perform a grid search with 10-fold cross-validation on different netwo
 import os
 
 neurons = [24, 32, 48, 64, 128, 256]
-layers = [1, 2, 3]
+layers = [1, 2]
 dropout = [0.1, 0.2]
 
 for l in layers:
     for n in neurons:
         for d in dropout:
             print("\nLayers: %i, Neurons: %i, Dropout: %.1f\n" % (l, n, d))
-            cmd = "python ~/PycharmProjects/LSTM_peptides/LSTM_peptides.py" + \
+            cmd = "python ~/PycharmProjects/LSTM_peptides/LSTM_peptides.py --window 40 --step 2" + \
                   " --layers %i --neurons %i --dropout %.1f --cv 10 --name cv10_l%i_n%i_d%.1f" % \
                   (l, n, d, l, n, d)
             os.system(cmd)
--- list_epochs.py
+++ list_epochs.py
@@ -5,11 +5,17 @@ import sys
 
 pwdir = sys.argv[1]
 
+print("Name,Epoch,Loss")
+
 for f in os.listdir(pwdir):
     if os.path.isdir(f) and f not in [".git", "logs", ".idea"]:
-        print(f)
         fdir = os.path.join(pwdir, f)
         for t in os.listdir(fdir):
-            if t.endswith("epoch.txt"):
-                os.system("cat %s" % os.path.join(fdir, t))
-                print("\n")
+            if t.endswith("best_epoch.txt"):
+                with open(os.path.join(fdir, t), 'r') as openf:
+                    next(openf)
+                    next(openf)
+                    reslt = list()
+                    for l in openf:
+                        reslt.append(l.strip().split("\t")[-1])
+                    print(f + ',' + ','.join(reslt))
